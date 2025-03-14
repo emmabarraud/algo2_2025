2268ebb0b7083bc176ba187f1b0a08fea0dcadab
emmabarraud <emma.barraud@gmail.com>
2025-03-13 23:29:44 -0300

    2330

--- lab00/bounds.c
+++ lab00/bounds.c
@@ -12,28 +12,32 @@ struct bound_data {
 
 struct bound_data check_bound(int value, int arr[], unsigned int length) {
 
-    struct bound_data res;
-    unsigned int i=0;
-    res.exists = false;
+    struct bound_data res = {false, false, false, 0}; // inicializo todas las variables en false
+
+    unsigned int i = 0; //variables auxiliares
+    int min = arr[0]; // empiezo en el primer elemento del arreglo
+    int max = arr[0]; 
 
     while (i < length) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+
         if (value == arr[i]) {
             res.exists = true;
-            res.is_upperbound = (true && res.is_upperbound);
-            res.is_lowerbound = (true && res.is_lowerbound);
             res.where = i;
-        } else if (value > arr[i]) {
-            res.is_upperbound = true;
-        } else {
-            res.is_lowerbound = true;
         }
         ++i;
     }
-    
+
+    res.is_upperbound = (value >= max); // asigno valores de verdad, comparo el valor dado con el valor calculado en el while
+    res.is_lowerbound = (value <= min);
 
     return res;
 }
-
 int main(void) {
 
     int tam;
@@ -42,8 +46,7 @@ int main(void) {
 
     int a[tam];
 
-    int n;
-    n=0; //inicializo la varriable 
+    int n=0;
     while (n<tam) {
         printf("\nIngrese el elemento numero %d del arreglo: \n", n);
         scanf("%d", &a[n]);
@@ -52,13 +55,13 @@ int main(void) {
 
     printf("El arreglo es: A[%d]={", tam);
     
-    n=0; //reinicio n
+    n=0;
     while (n<tam) {
         if(n < tam - 1){
-            printf("%d, ", a[n]); //salida con coma
+            printf("%d, ", a[n]);
         }else{
-            printf("%d", a[n]); // salida sin coma para cerrar el arreglo
-        } 
+            printf("%d", a[n]); 
+        }
         ++n;
     }
     printf("}\n\n");
@@ -68,7 +71,7 @@ int main(void) {
     scanf("%d", &value);
 
     struct bound_data result = check_bound(value, a, tam);
-
+    int lugar = result.where;
 /*    if ((result.exists == true) && (result.is_upperbound == true)) {
         printf("El valor ingresado '%d' es el maximo de la lista\n");
      };
@@ -87,17 +90,17 @@ MAS OPTIMIZADO
 
     if (result.exists) { //resultado.exists es verdadero
         if (result.is_upperbound) {
-            printf("El valor ingresado '%d' es el máximo de la lista\n EL lu");
+            printf("El valor ingresado '%d' es el máximo de la lista\n El lugar en el que se encuentra es %d", value, lugar);
         } 
         if (result.is_lowerbound) {
-            printf("El valor ingresado '%d' es el mínimo de la lista\n");
+            printf("El valor ingresado '%d' es el mínimo de la lista\n El lugar en el que se encuentra es %d", value, lugar);
         }
     } else {  //resultado.exists es falso
         if (result.is_lowerbound) {
-            printf("El valor ingresado '%d' es la cota menor\n");
+            printf("El valor ingresado '%d' es la cota menor (no esta en el arreglo)\n", value);
         } 
         if (result.is_upperbound) {
-            printf("El valor ingresado '%d' es la cota mayor\n");
+            printf("El valor ingresado '%d' es la cota mayor (no esta en el arreglo)\n", value);
         }
     }
     
